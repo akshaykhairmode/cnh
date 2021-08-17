6b7a7166ab92f07c2cf936990ac492b56e4490d0
Akshay Khairmode <akshaykhairmode@gmail.com>
2021-08-18 00:34:57 +0530

    Refactor

--- 1_christmas.go
+++ 1_christmas.go
@@ -15,11 +15,13 @@ func main() {
 		panic("Could not parse the date, error : " + err.Error())
 	}
 
+	finalTime := christmas.Sub(time.Now())
+
 	fmt.Printf("Milliseconds to christmas: %v\n", christmas.Sub(time.Now()).Milliseconds())
 
 	//%.f will truncate the decimals, we dont need them for printing purpose.
-	fmt.Printf("Seconds to christmas: %.f\n", christmas.Sub(time.Now()).Seconds())
-	fmt.Printf("Minutes to christmas: %.f\n", christmas.Sub(time.Now()).Minutes())
-	fmt.Printf("Hours to christmas: %.f\n", christmas.Sub(time.Now()).Hours())
-	fmt.Printf("Days to christmas: %.f\n", math.Round(christmas.Sub(time.Now()).Hours()/24))
+	fmt.Printf("Seconds to christmas: %.f\n", finalTime.Seconds())
+	fmt.Printf("Minutes to christmas: %.f\n", finalTime.Minutes())
+	fmt.Printf("Hours to christmas: %.f\n", finalTime.Hours())
+	fmt.Printf("Days to christmas: %.f\n", math.Round(finalTime.Hours()/24))
 }
